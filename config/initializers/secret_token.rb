f459914af1d79d58d9308870cac4c109e54f538f
Emily Williams <ecwilliams66@gmail.com>
2014-04-03 10:56:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tracktivity::Application.config.secret_key_base = 'ceeac0dccf514fd14fe98dd745ea9a30060b6786afb8a77e23f756727f98f2805dc253ffa62960634767630bac36309bbc0a87c7091247a20836f2314398fdc5'

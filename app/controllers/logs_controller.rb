7f1a7a579215dd6c5a1af97b6a879550bdbb4d67
Emily Williams <ecwilliams66@gmail.com>
2014-04-27 19:29:08 -0400

    Got rid of binding.pry :x

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -9,7 +9,6 @@ class LogsController < ApplicationController
           @queried_logs       = @logs.sort_by_week(query_date)
           @queried_activities = @queried_logs.list_activities
           @total_productivity = @queried_logs.total_productivity
-          binding.pry
           @queried_categories = @queried_logs.list_categories
           render :index
         else
